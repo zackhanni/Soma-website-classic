169fa3753d6b49499573aa8d87104415fcb3b1f7
Ariel Kirkwood <ariel.kirkwood@me.com>
2016-04-17 16:14:23 -0400

    Change page scroll transition

--- js/grayscale.js
+++ js/grayscale.js
@@ -19,7 +19,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1000, 'easeInOutExpo');
+        }, 600, 'easeInOutExpo');
         event.preventDefault();
     });
 });
